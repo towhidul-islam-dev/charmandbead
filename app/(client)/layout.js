c3aa0d6d9cf8dbdd741f119ff8069123b0ccba2a
towhidul-islam-dev <niamaaktar@gmail.com>
2026-02-18 22:16:25 +0600

    fix the favicon

--- app/(client)/layout.js
+++ app/(client)/layout.js
@@ -32,7 +32,7 @@ export const metadata = {
     images: [{ url: '/og-image.jpg' }],
   },
   icons: {
-    icon: "/favicon.svg", // Path to file in your /public folder
+    icon: "/favicon1.svg", // Path to file in your /public folder
   },
 };
 
