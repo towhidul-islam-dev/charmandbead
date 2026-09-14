c1358df5ac9fc54e579539bc620a0cb328b73635
towhidul-islam-dev <niamaaktar@gmail.com>
2026-09-14 10:45:32 +0600

    fix the text visibility in the serach component

--- components/admin/SearchUsers.js
+++ components/admin/SearchUsers.js
@@ -23,7 +23,7 @@ export default function SearchUsers({ placeholder }) {
       <label htmlFor="search" className="sr-only">Search</label>
       <Search className="absolute left-4 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-400 group-focus-within:text-[#EA638C] transition-colors" />
       <input
-        className="peer block w-full md:w-80 rounded-2xl border border-gray-200 py-[14px] pl-11 text-[11px] font-bold placeholder:text-gray-400 outline-none focus:border-[#EA638C] focus:ring-4 focus:ring-[#EA638C]/5 transition-all bg-white"
+        className="peer block w-full md:w-80 rounded-2xl border border-gray-200 py-[14px] pl-11 text-[11px] font-bold text-[#3E442B] placeholder:text-gray-400 outline-none focus:border-[#EA638C] focus:ring-4 focus:ring-[#EA638C]/5 transition-all bg-white"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
         defaultValue={searchParams.get("query")?.toString()}
